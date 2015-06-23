85044b7090910448bf085efa1dcefc57c4339302
Rahat Ahmed <rahatarmanahmed@gmail.com>
2015-06-23 11:30:29 -0500

    Initial commit.
    Loading styles and watching for changes to hot load remain to be implemented.

---
+++ index.js
@@ -0,0 +1,27 @@
+var LessCache = require('less-cache');
+var os = require('os');
+// var fs = require('fs');
+
+module.exports = function(opts) {
+	opts = opts || {};
+	var cacheDir = opts.cacheDir || os.tmpdir();
+	var cache = new LessCache({cacheDir: cacheDir});
+
+	var load = function(path) {
+		return cache.readFileSync(path);
+	};
+
+	load.style = function(path) {
+		var css = load(path);
+		// TODO: make sure document exists, create style tag
+		throw new Error('Not yet implemented');
+	};
+
+	load.style.hot = function(path) {
+		var style = style(path);
+		// TODO: add watcher on less file and swap in new compiled less
+		throw new Error('Not yet implemented');
+	};
+
+	return load;
+};
